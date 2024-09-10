50ce3c9acf5692b296fda0a621a70a03ba9c0ce9
Ankur1441 <129702538+Ankur1441@users.noreply.github.com>
2024-09-10 10:22:40 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2==2);
+// console.log(2!=2);
+// console.log(2>2);
+// console.log(2<2);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// comparison operator converts the null to a number and treating it as zero(0).****
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0); // these three always return false
+
+// === -> it strictly check the values
+console.log("2" === 2);
+
