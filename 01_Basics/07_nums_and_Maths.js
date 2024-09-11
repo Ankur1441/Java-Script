f247fca4b32b8c73e817c8b2bea83a42353ba602
Ankur1441 <129702538+Ankur1441@users.noreply.github.com>
2024-09-11 05:24:59 +0000

    nums_and_maths

---
+++ 01_Basics/07_nums_and_Maths.js
@@ -0,0 +1,14 @@
+const score = 400
+console.log(score);
+ 
+const num = new Number(100)
+console.log(num);
+console.log(num.toString());
+console.log(typeof(num));
+console.log(num.toFixed(2));
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(4));
+console.log(otherNumber.toFixed(2));
+
+
