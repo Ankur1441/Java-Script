86414450354820c734f9d2586063d70eff3192e6
Ankur1441 <129702538+Ankur1441@users.noreply.github.com>
2024-09-11 11:30:44 +0000

    nums_and_Maths

--- 01_Basics/07_nums_and_Maths.js
+++ 01_Basics/07_nums_and_Maths.js
@@ -1,14 +1,39 @@
 const score = 400
-console.log(score);
+// console.log(score);
  
 const num = new Number(100)
-console.log(num);
-console.log(num.toString());
-console.log(typeof(num));
-console.log(num.toFixed(2));
+// console.log(num);
+// console.log(num.toString());
+// console.log(typeof(num));
+// console.log(num.toFixed(2));
 
-const otherNumber = 123.8966
-console.log(otherNumber.toPrecision(4));
-console.log(otherNumber.toFixed(2));
+// const otherNumber = 123.8966
+// console.log(otherNumber.toFixed(2));
+// console.log(otherNumber.toPrecision(4));
 
 
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+//<---------------------Maths----------------------------->
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,7,8));
+// console.log(Math.max(4,3,7,8));
+
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1))+min)
